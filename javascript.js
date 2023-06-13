67b40360754d302cffca70d989158acba9c03c94
rteixeiragarcia <rteixeiragarcia@gmail.com>
2023-06-13 11:29:03 -0300

    Create function for player weapon against computer

--- javascript.js
+++ javascript.js
@@ -19,3 +19,51 @@ let updateScore = (classPlayer, score) => {
     const textScore = document.querySelector(classPlayer);
     textScore.textContent = `${score}`;
 }
+    
+let playRock = () => {
+    let choice = getComputerChoice();
+
+    switch (choice) {
+        case "rock":
+            return "tie";
+            break;
+        case "paper":
+            return "lose";
+            break;
+        case "scissors":
+            return "win";
+            break;
+    }
+}
+
+let playPaper = () => {
+    let choice = getComputerChoice();
+
+    switch (choice) {
+        case "rock":
+            return "win";
+            break;
+        case "paper":
+            return "tie";
+            break;
+        case "scissors":
+            return "lose";
+            break;
+    }
+}
+
+let playScissors = () => {
+    let choice = getComputerChoice();
+
+    switch (choice) {
+        case "rock":
+            return "lose";
+            break;
+        case "paper":
+            return "win";
+            break;
+        case "scissors":
+            return "tie";
+            break;
+    }
+}
