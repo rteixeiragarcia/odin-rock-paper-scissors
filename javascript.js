647cfc102596bceff508590e0b28dc40b6c8066e
rteixeiragarcia <rteixeiragarcia@gmail.com>
2023-06-13 14:36:54 -0300

    Style color for win, lose and draw rounds

--- javascript.js
+++ javascript.js
@@ -32,29 +32,43 @@ let playRock = () => {
 
     switch (choice) {
         case "rock":
+            scissorsBtn.style.cssText = "box-shadow: none";
+            paperBtn.style.cssText = "box-shadow: none";
             resultRound.textContent = "It's a tie. Rock ties with Rock.";
             updateScore("player-score", playerScore);
             updateScore("computer-score", computerScore);
             compImg.setAttribute("src", "images/fist.png");
             computerBtn.appendChild(compImg);
+            rockBtn.style.cssText = "box-shadow: 0px 0px 30px yellow;";
+            computerBtn.style.cssText = "box-shadow: 0px 0px 30px yellow;"
             return "tie";
             break;
+
         case "paper":
+            scissorsBtn.style.cssText = "box-shadow: none";
+            paperBtn.style.cssText = "box-shadow: none";
             resultRound.textContent = "You lose! Paper beats Rock."
             ++computerScore;
             updateScore("player-score", playerScore);
             updateScore("computer-score", computerScore);
             compImg.setAttribute("src", "images/hand.png");
             computerBtn.appendChild(compImg);
+            rockBtn.style.cssText = "box-shadow: 0px 0px 30px red;";
+            computerBtn.style.cssText = "box-shadow: 0px 0px 30px green;"
             return "lose";
             break;
+
         case "scissors":
+            scissorsBtn.style.cssText = "box-shadow: none";
+            paperBtn.style.cssText = "box-shadow: none";
             resultRound.textContent = "You win! Rock beats Scissors."
             ++playerScore;
             updateScore("player-score", playerScore);
             updateScore("computer-score", computerScore);
             compImg.setAttribute("src", "images/two.png");
             computerBtn.appendChild(compImg);
+            rockBtn.style.cssText = "box-shadow: 0px 0px 30px green;";
+            computerBtn.style.cssText = "box-shadow: 0px 0px 30px red;"
             return "win";
             break;
     }
@@ -65,29 +79,43 @@ let playPaper = () => {
 
     switch (choice) {
         case "rock":
+            scissorsBtn.style.cssText = "box-shadow: none";
+            rockBtn.style.cssText = "box-shadow: none";
             resultRound.textContent = "You win! Paper beats Rock.";
             ++playerScore;
             updateScore("player-score", playerScore);
             updateScore("computer-score", computerScore);
             compImg.setAttribute("src", "images/fist.png");
             computerBtn.appendChild(compImg);
+            paperBtn.style.cssText = "box-shadow: 0px 0px 30px green;";
+            computerBtn.style.cssText = "box-shadow: 0px 0px 30px red;"
             return "win";
             break;
+
         case "paper":
+            scissorsBtn.style.cssText = "box-shadow: none";
+            rockBtn.style.cssText = "box-shadow: none";
             resultRound.textContent = "It's a tie. Paper ties with Paper.";
             updateScore("player-score", playerScore);
             updateScore("computer-score", computerScore);
             compImg.setAttribute("src", "images/hand.png");
             computerBtn.appendChild(compImg);
+            paperBtn.style.cssText = "box-shadow: 0px 0px 30px yellow;";
+            computerBtn.style.cssText = "box-shadow: 0px 0px 30px yellow;"
             return "tie";
             break;
+
         case "scissors":
+            scissorsBtn.style.cssText = "box-shadow: none";
+            rockBtn.style.cssText = "box-shadow: none";
             resultRound.textContent = "You lose! Scissors beats Paper.";
             ++computerScore;
             updateScore("player-score", playerScore);
             updateScore("computer-score", computerScore);
             compImg.setAttribute("src", "images/two.png");
             computerBtn.appendChild(compImg);
+            paperBtn.style.cssText = "box-shadow: 0px 0px 30px red;";
+            computerBtn.style.cssText = "box-shadow: 0px 0px 30px green;"
             return "lose";
             break;
     }
@@ -98,29 +126,43 @@ let playScissors = () => {
 
     switch (choice) {
         case "rock":
+            paperBtn.style.cssText = "box-shadow: none";
+            rockBtn.style.cssText = "box-shadow: none";
             resultRound.textContent = "You lose! Rock beats Scissors";
             ++computerScore;
             updateScore("player-score", playerScore);
             updateScore("computer-score", computerScore);
             compImg.setAttribute("src", "images/fist.png");
             computerBtn.appendChild(compImg);
+            scissorsBtn.style.cssText = "box-shadow: 0px 0px 30px red;";
+            computerBtn.style.cssText = "box-shadow: 0px 0px 30px green;"
             return "lose";
             break;
+
         case "paper":
+            paperBtn.style.cssText = "box-shadow: none";
+            rockBtn.style.cssText = "box-shadow: none";
             resultRound.textContent = "You win! Scissors beats Paper";
             ++playerScore;
             updateScore("player-score", playerScore);
             updateScore("computer-score", computerScore);
             compImg.setAttribute("src", "images/hand.png");
             computerBtn.appendChild(compImg);
+            scissorsBtn.style.cssText = "box-shadow: 0px 0px 30px green;";
+            computerBtn.style.cssText = "box-shadow: 0px 0px 30px red;"
             return "win";
             break;
+
         case "scissors":
+            paperBtn.style.cssText = "box-shadow: none";
+            rockBtn.style.cssText = "box-shadow: none";
             resultRound.textContent = "It's a tie. Scissors ties with Scissors.";
             updateScore("player-score", playerScore);
             updateScore("computer-score", computerScore);
             compImg.setAttribute("src", "images/two.png");
             computerBtn.appendChild(compImg);
+            scissorsBtn.style.cssText = "box-shadow: 0px 0px 30px yellow;";
+            computerBtn.style.cssText = "box-shadow: 0px 0px 30px yellow;"
             return "tie";
             break;
     }
