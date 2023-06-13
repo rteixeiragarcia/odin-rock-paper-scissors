b687b1f40725a706268ba705adc186ffb658b88e
rteixeiragarcia <rteixeiragarcia@gmail.com>
2023-06-13 11:05:12 -0300

    Change functions to arrow functions

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ textPlayerScore.textContent = `${playerScore}`;
 const textComputerScore = document.querySelector(".computer-score");
 textComputerScore.textContent = `${computerScore}`;
 
-function getComputerChoice() {
+let getComputerChoice = () => {
     let options = ["rock", "paper", "scissors"];
 
     let optionRandom = Math.floor(Math.random() * 3);
@@ -15,7 +15,7 @@ function getComputerChoice() {
     return options[optionRandom];
 }
 
-function updateScore(classPlayer, score) {
+let updateScore = (classPlayer, score) => {
     const textScore = document.querySelector(classPlayer);
     textScore.textContent = `${score}`;
 }
